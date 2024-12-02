602193d584bad33667b25dbadc3c2b30bd5ff5bc
Ignacio Martin Balletbo <nachomartin237@gmail.com>
2024-12-02 20:40:22 -0300

    primer guardado

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,23 @@
+import header from './components/header'
+import body from './components/body'
+import footer from './components/footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <header>
+
+      </header>
+      <body>
+        
+      </body>
+      <footer>
+        
+      </footer>
+    </>
+  )
+}
+
+export default App
